eedf0b6cf1c9c61fc8129f6a697714874c99f68c
Nikita <krylov.nikita.a@yandex.ru>
2017-11-05 22:37:39 +0300

    Initial commit

---
+++ Particles.shader
@@ -0,0 +1,77 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Subway/Particles"
+{
+
+    Properties
+    {
+        _ColorLow("Color Slow Speed", Color) = (0, 0, 0.5, 1)
+        _ColorHigh("Color High Speed", Color) = (1, 0, 0, 1)
+        _HighSpeedValue("High speed Value", Range(0, 50)) = 25
+    }
+
+    SubShader
+    {
+        Pass
+        {
+            Blend SrcAlpha one
+
+            CGPROGRAM
+            #pragma target 5.0
+
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            // Particle's data
+            struct Particle
+            {
+                float2 position;
+                float2 velocity;
+            };
+
+            // Pixel shader input
+            struct PS_INPUT
+            {
+                float4 position : SV_POSITION;
+                float4 color : COLOR;
+            };
+
+            // Particle's data, shared with the compute shader
+            StructuredBuffer<Particle> Particles;
+
+            // Properties variables
+            uniform float4 _ColorLow;
+            uniform float4 _ColorHigh;
+            uniform float _HighSpeedValue;
+
+            // Vertex shader
+            PS_INPUT vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID)
+            {
+                PS_INPUT o = (PS_INPUT)0;
+
+                // Color
+                float speed = length(Particles[instance_id].velocity);
+                float lerpValue = clamp(speed / _HighSpeedValue, 0.0f, 1.0f);
+                o.color = lerp(_ColorLow, _ColorHigh, lerpValue);
+                // Position
+                o.position = UnityObjectToClipPos(float4(Particles[instance_id].position, 0.0f, 1.0f));
+
+                return o;
+            }
+
+            // Pixel shader
+            float4 frag(PS_INPUT i) : COLOR
+            {
+                return i.color;
+            }
+
+            ENDCG
+        }
+    }
+
+    Fallback Off
+}
---
+++ ParticlesManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ParticlesManager : MonoBehaviour {
+    public struct Particle
+    {
+        public Vector2 position;
+        public Vector2 velocity;
+    }
+
+    public ComputeShader computeShader;
+    public Material material;
+
+    ComputeBuffer particles;
+
+    const int WARP_SIZE = 1024;
+
+    int size = 1024000;
+    int stride;
+
+    int warpCount;
+
+    int kernelIndex;
+
+    Particle[] initBuffer;
+
+    /*private void OnDrawGizmos()
+    {
+        var p = new Particle[size];
+        particles.GetData(p);
+        for (int i = 0; i < size; i++)
+        {
+            Gizmos.DrawSphere(p[i].position, 0.1f);
+        }
+    }*/
+
+    // Use this for initialization
+    void Start () {
+
+        warpCount = Mathf.CeilToInt((float)size / WARP_SIZE);
+
+        stride = Marshal.SizeOf(typeof(Particle));
+        particles = new ComputeBuffer(size, stride);
+
+        initBuffer = new Particle[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            initBuffer[i] = new Particle();
+            initBuffer[i].position = Random.insideUnitCircle * 10f;
+            initBuffer[i].velocity = Vector2.zero;
+        }
+
+        particles.SetData(initBuffer);
+
+        kernelIndex = computeShader.FindKernel("Update");
+        
+        computeShader.SetBuffer(kernelIndex, "Particles", particles);
+
+        material.SetBuffer("Particles", particles);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            particles.SetData(initBuffer);
+        }
+
+        computeShader.SetInt("shouldMove", Input.GetMouseButton(0) ? 1 : 0);
+        var mousePosition = GetMousePosition();
+        computeShader.SetFloats("mousePosition", mousePosition);
+        computeShader.SetFloat("dt", Time.deltaTime);
+        computeShader.Dispatch(kernelIndex, warpCount, 1, 1);
+	}
+
+    float[] GetMousePosition()
+    {
+        var mp = Input.mousePosition;
+        var v = Camera.main.ScreenToWorldPoint(mp);
+        return new float[] { v.x, v.y };
+    }
+
+    void OnRenderObject()
+    {
+        material.SetPass(0);
+        Graphics.DrawProcedural(MeshTopology.Points, 1, size);
+    }
+
+    void OnDestroy()
+    {
+        if (particles != null)
+            particles.Release();
+    }
+}
